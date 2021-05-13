e2422cfc7b648cc97075a754f5aa72e6bfed00a9
Théo Vidal <theo@becauseofprog.fr>
2021-05-13 19:38:40 +0200

    feat: add coordinates stack to Arduino code and create real tests

--- hardware/hardware.ino
+++ hardware/hardware.ino
@@ -20,9 +20,9 @@ int deg_lidar = 0;
 float rad_body = 0;
 float rad_lidar = 0;
 
-float x = 0;
-float y = 0;
-float z = 0;
+int x_stack[181] = {};
+int y_stack[181] = {};
+int z_stack[181] = {};
 
 float degToRad(int n) {
   return n * PI / 180;
@@ -34,40 +34,41 @@ void getParts(float n, int *en, int *dec) {
 }
 
 void sendData() {
-  int x_en, x_dec;
-  getParts(x, &x_en, &x_dec);
-
-  int y_en, y_dec;
-  getParts(y, &y_en, &y_dec);
-
-  int z_en, z_dec;
-  getParts(z, &z_en, &z_dec);
-
-  int data[6] = {
-    x_en, x_dec,
-    y_en, y_dec,
-    z_en, z_dec
-  };
-
-  xbee.write(1);
-  for (int i = 0; i < 6; i++) {
-     int n = data[i];
-     xbee.write((n >> 8) & 0xFF);
-     xbee.write(n & 0xFF);
+  for (int deg = 0; deg <= 180; deg++) {
+    int x_en, x_dec;
+    getParts(x_stack[deg], &x_en, &x_dec);
+  
+    int y_en, y_dec;
+    getParts(y_stack[deg], &y_en, &y_dec);
+  
+    int z_en, z_dec;
+    getParts(z_stack[deg], &z_en, &z_dec);
+  
+    int data[6] = {
+      x_en, x_dec,
+      y_en, y_dec,
+      z_en, z_dec
+    };
+  
+    xbee.write(1);
+    for (int i = 0; i < 6; i++) {
+       int n = data[i];
+       xbee.write((n >> 8) & 0xFF);
+       xbee.write(n & 0xFF);
+    }
+    xbee.write((byte)(0));
+    delay(20); 
   }
-  xbee.write((byte)(0));
-  delay(20);
 }
 
 void getCoordinates() {
-  distance = lidar.distance() + 3;
+  distance = lidar.distance() - 1;
   rad_lidar = degToRad(deg_lidar);
   rad_body = degToRad(deg_body);
   
-  x = distance * cos(rad_lidar) * sin(rad_body);
-  y = distance * sin(rad_lidar);
-  z = distance * cos(rad_lidar) * cos(rad_body);
-  delay(15);
+  x_stack[deg_lidar] = distance * cos(rad_lidar) * sin(rad_body);
+  y_stack[deg_lidar] = distance * sin(rad_lidar);
+  z_stack[deg_lidar] = distance * cos(rad_lidar) * cos(rad_body);
 }
 
 void setup() {
@@ -87,15 +88,11 @@ void loop() {
     for (deg_lidar = 0; deg_lidar <= 180; deg_lidar++) {
       servo_lidar.write(deg_lidar);
       delay(15);
-
       getCoordinates();
-      sendData();
-     
-      Serial.println(deg_lidar);
     }
     
     servo_lidar.write(0);
-    delay(1000);
+    sendData();
   }
 
   servo_body.write(0);
